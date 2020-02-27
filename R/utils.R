518d76b969b4f8e05f429d242d110a94afae6f28
mstaniak <mtst@mstaniak.pl>
2020-02-27 22:32:11 +0100

    updates MaxQuant converter

--- R/utils.R
+++ R/utils.R
@@ -304,12 +304,19 @@
     data_frame
 }
 
-.handleDecoyProteins = function(data_frame, decoy_column, decoy_symbols, drop = TRUE) {
-    decoy_index = which(colnames(data_frame) == decoy_column)
-    if(drop) {
-        data_frame[!(data_frame[[decoy_column]] %in% decoy_symbols), -decoy_index]    
+.handleDecoyProteins = function(data_frame, decoy_column, decoy_symbols, 
+                                drop = TRUE, filter = TRUE) {
+    decoy_index = which(colnames(data_frame) == decoy_column)  
+    if(filter) {
+        decoy_filter = !(data_frame[[decoy_column]] %in% decoy_symbols)
     } else {
-        data_frame[!(data_frame[[decoy_column]] %in% decoy_symbols), ]
+        decoy_filter = rep(TRUE, nrow(data_frame))
+    }
+    
+    if(drop) {
+        data_frame[decoy_filter, -decoy_index, with = FALSE]    
+    } else {
+        data_frame[decoy_filter, ]
     }
 }
 
@@ -529,4 +536,54 @@
                  "FragmentIon", "ProductCharge", "IsotopeLabelType",
                  "Condition", "BioReplicate", "Run", "Intensity"),
              with = FALSE]
+}
+
+.filterManyColumns = function(data_frame, filter_columns, filter_symbols) {
+    for(col in filter_columns) {
+        has_col = is.element(col, colnames(data_frame))
+        data_frame = .handleDecoyProteins(data_frame, col, filter_symbols, 
+                                          has_col, has_col)        
+    }
+    data_frame
+}
+
+.cleanRawMaxQuant = function(mq_input, mq_pg) {
+    mq_input = data.table::as.data.table(mq_input)
+    mq_pg = data.table::as.data.table(mq_pg)
+    
+    filter_cols = c("Contaminant", "Potential.contaminant", 
+                    "Reverse", "Only.identified.by.site")
+    mq_input = .filterManyColumns(mq_input, filter_cols, "+")
+    mq_pg = .filterManyColumns(mq_pg, filter_cols, "+")
+    mq_input[["Protein.group.IDs"]] = as.integer(as.character(mq_input[["Protein.group.IDs"]]))
+    # Need to check if 'id' in proteinGroups.txt and 'Protein.group.IDs' in evidence are the same. 
+    # Possible to have some combination in Protein.group.IDs in infile, such as 64;1274;1155;1273 instead of 64, 1274.. separately. 
+    # Combination of some ids seems not to be used for intensity
+    # TODO: move this comment to docs.
+    ## then take proteins which are included
+    mq_input = mq_input[mq_input[["Protein.group.IDs"]] %in% unique(mq_pg[["id"]]), ]
+    ## then use 'protein.IDs' in proteinGroups.txt
+    ## because if two 'proteins' in evidence.txt are used in one protein ID, need to use certain protein name in infile.
+    ## for example, protein.IDs in proteinGroups.txt are P05204;O00479. but, two 'proteins in evidence.txt, such as P05204;O00479, and P05204.
+    # TODO: move this comment to docs
+    protein_names = unique(mq_pg[,c("Protein.IDs", "id")])
+    colnames(protein_names) = c("uniqueProteins", "Protein.group.IDs")
+    mq_input = merge(mq_input, protein_names, by = "Protein.group.IDs")
+    
+    mq_cols = c("Sequence", "Modified.sequence", 
+                "Modifications", "Charge", "Run", "Intensity", 
+                "Fraction", "TechReplicate", "Raw.file") 
+    # TODO: + Retention.time? - Modifications?
+    protein_id = ifelse(proteinID == "Proteins", "uniqueProteins",
+                        "Leading.razor.protein")
+    mq_cols = intersect(c(mq_cols, protein_id),
+                        colnames(mq_input))
+    mq_input = mq_input[, mq_cols, with = FALSE]
+    mq_input[["Modified.sequence"]] = gsub("_", "", mq_input[["Modified.sequence"]])
+    mq_input = mq_input[!is.na(mq_input[["Intensity"]]), ]
+    colnames(mq_input) = .updateColnames(
+        mq_input, 
+        c(protein_id, "Modified.sequence", "Charge", "Raw.file"), 
+        c("ProteinName", "PeptideSequence", "PrecursorCharge", "Run"))
+    mq_input
 }
