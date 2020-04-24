6cd98e2eae4d396f91f87085f1d36eb640b95173
mstaniak <mtst@mstaniak.pl>
2020-04-25 00:35:40 +0200

    updates Skyline converter

--- R/convert_Skyline.R
+++ R/convert_Skyline.R
@@ -14,98 +14,59 @@
 #' @export
 #' 
 
-.SkylineFromFiles = function(
+SkylinetoMSstatsFormat = function(
     input, annotation = NULL, removeiRT = TRUE, filter_with_Qvalue = TRUE,
     qvalue_cutoff = 0.01, useUniquePeptide = TRUE, fewMeasurements = "remove",
     removeOxidationMpeptides = FALSE, removeProtein_with1Feature = FALSE,
     use_log_file = TRUE, append = FALSE, verbose = TRUE
 ) {
+    .setMSstatsLogger(use_log_file, append, verbose)
     
-    fewMeasurements = .isLegalValue(fewMeasurements, legal_values = c("remove", "keep"))
+    # fewMeasurements = .isLegalValue(fewMeasurements, legal_values = c("remove", "keep"))
     
-    input = .getInput(input)
-    input = .cleanRawSkyline(input)
     annotation = .makeAnnotation(
         annotation, 
         c("Run" = "Run", "Condition" = "Condition", "BioReplicate" = "BioReplicate"),
         input
     )
-    input = .handleDecoyProteins(input, "ProteinName", c("DECOY", "Decoys"), FALSE)
-    if(removeiRT) {
-        input = .handleDecoyProteins(input, "StandardType", "iRT", FALSE)
-        # TODO: maybe refactor later to match style of .handleSharedPeptides etc
-    }
-    input = .handleOxidationPeptides(input, "PeptideSequence", "+16", removeOxidationMpeptides)
+    input = .cleanRawSkyline(input)
+    input = .handleDecoyProteins(input, "ProteinName", 
+                                 c("DECOY", "Decoys"), FALSE)
+    input = .handleDecoyProteins(input, "StandardType", "iRT", FALSE, removeiRT)
+    input = .handleOxidationPeptides(input, "PeptideSequence", "+16", 
+                                     removeOxidationMpeptides)
     input = .handleSharedPeptides(input, useUniquePeptide)
-    input = .handleFiltering(input, "Truncated", 0L, "smaller", "fill", NA_real_) # trick
-    
-    if (.checkDDA(input)) {
-        input = .aggregateMonoisotopicPeaks(input)
-        # TODO: maybe replace with .summarizeMultipleMeasurements (performance and logic). Can this be done later inside .summarize...?
-    }
-    
-    input = .handleFiltering(input, "DetectionQValue", qvalue_cutoff, "smaller", "fill", 0.01, FALSE, filter_with_Qvalue)
+    input = .handleFiltering(input, "Truncated", 0L, "smaller", "fill", NA_real_)
+    input = .handleIsotopicPeaks(input)
+    input = .handleFiltering(input, "DetectionQValue", qvalue_cutoff, "smaller", 
+                             "fill", 0.01, FALSE, filter_with_Qvalue)
     # TODO: conditionally check if DetectionQValue is in the input at the beginning
-    feature_cols = c("PeptideSequence", "PrecursorCharge", "FragmentIon", "ProductCharge")
+    feature_cols = c("PeptideSequence", "PrecursorCharge", 
+                     "FragmentIon", "ProductCharge")
     input = .cleanByFeature(input, feature_cols, max, fewMeasurements)
-    input = .handleSingleFeaturePerProtein(input, removeProtein_with1Feature)
-    input <- merge(input[, setdiff(colnames(input), c("Condition", "BioReplicate")), with = FALSE], annotation, by = "Run")
-    input # TODO: change ProteinName to facor?
+    input = .handleSingleFeaturePerProtein(input, removeProtein_with1Feature,
+                                           feature_cols)
+    input = merge(input, annotation, by = "Run")
+    input
 }
 
 
-.OpenMSFromFiles = function(
-    input, annotation = NULL, useUniquePeptide = TRUE, fewMeasurements = "remove",
-    removeProtein_with1Feature = FALSE, summaryforMultipleRows = max,
-    use_log_file = TRUE, append = FALSE, verbose = TRUE
-) {
-    
-}
-
-setGeneric("SkylinetoMSstatsFormat",
-           function(input, annotation, ...) {
-               standardGeneric("SkylinetoMSstatsFormat")
-           })
-
-setMethod("SkylinetoMSstatsFormat",
-          signature = rep("character", 2),
-          function(input, annotation, ...)
-              .SkylineFromFiles(input, annotation, ...))
-
-
-setMethod("SkylinetoMSstatsFormat",
-          signature = rep("data.frame", 2),
-          function(input, annotation, ...) {
-              .SkylineFromDFs(input, annotation, ...)
-          })
-
-setMethod("SkylinetoMSstatsFormat",
-          signature = c("data.frame", "missing"),
-          function(input, ...) {
-              .SkylineFromDFs(input, ...)
-          })
-
-setMethod("SkylinetoMSstatsFormat",
-          signature = c("character", "missing"),
-          function(input, ...) {
-              .OpenMSFromFiles(input, ...)
-          })
-
-
+#' Clean raw data from Skyline
+#' @param sl_input data.frame with raw Skyline input or a path to Skyline output.
+#' @return data.table
+#' @keywords internal
 .cleanRawSkyline = function(sl_input) {
+    sl_input = .getDataTable(sl_input)
     colnames(sl_input) = gsub("\\.", "", colnames(sl_input))
     colnames(sl_input) = .updateColnames(sl_input, c("FileName", "Area"),
                                          c("Run", "Intensity"))
-    colnames(sl_input) = .standardizeColnames(sl_input)
+    colnames(sl_input) = .standardizeColnames(colnames(sl_input))
     
-    sl_input = data.table::as.data.table(sl_input) 
-    if(is.element("PeptideSequence", colnames(sl_input))) {
-        sl_input = sl_input[, .(PeptideSequence) := NULL]
-    }
+    sl_input = sl_input[, !(colnames(sl_input) == "PeptideSequence"), with = FALSE]
     colnames(sl_input) = .updateColnames(sl_input, "PeptideModifiedSequence",
                                          "PeptideSequence")
     sl_input[["Intensity"]] = as.numeric(sl_input[["Intensity"]])
-    if(is.element("DetectionQValue", colnames(sl_input))) {
+    if (is.element("DetectionQValue", colnames(sl_input))) {
         sl_input[["DetectionQValue"]] = as.numeric(as.character(sl_input[["DetectionQValue"]]))    
     }
     if(is.character(sl_input[["Truncated"]])) {
@@ -118,5 +79,46 @@ setMethod("SkylinetoMSstatsFormat",
                 "BioReplicate", "Run", "Intensity", "StandardType")
     sl_cols = c(sl_cols, "Fraction", "DetectionQValue", "Truncated")
     sl_input = sl_input[, intersect(sl_cols, colnames(sl_input)), with = FALSE]
-    sl_input
+    sl_input[, setdiff(colnames(sl_input), c("Condition", "BioReplicate")), 
+             with = FALSE]
+}
+
+
+#' Handle isotopic peaks
+#' @param input data.table preprocessed by one of the `cleanRaw*` functions.
+#' @return data.table
+#' @keywords internal
+.handleIsotopicPeaks = function(input) {
+    if (.checkDDA(input)) {
+        input = .summarizeMultipleMeasurements(input, 
+                                               function(x) sum(x, na.rm = TRUE),
+                                               c("PeptideSequence",
+                                                 "PrecursorCharge"))
+    }
+    getOption("MSstatsLog")("INFO", "Three isotopic preaks per feature and run are summed")
+    getOption("MSstatsMsg")("INFO", "Three isotopic preaks per feature and run are summed")
+    input
+}
+
+
+#' Check validity of DDA data
+#' @inherit .handleIsotopicPeaks
+#' @return logical
+#' @keywords internal
+.checkDDA = function(input) {
+    fragment_ions = as.character(unique(input[["FragmentIon"]]))
+    check_DDA = setdiff(c("precursor", "precursor [M+1]", "precursor [M+2]"), 
+                        fragment_ions)
+    frags = setdiff(fragment_ions, 
+                    c('precursor', 'precursor [M+1]', 'precursor [M+2]'))
+    precursors = intersect(fragment_ions, 
+                           c("precursor", "precursor [M+1]", "precursor [M+2]"))
+    if (length(frags) > 0 & length(precursors) > 0) {
+        msg = paste("Please check precursors information.",
+                    "If your experiment is DIA, please remove the precursors.",
+                    "If your experiments is DDA, please check the precursor information.")
+        getOption("MSstatsLog")("ERROR", msg)
+        stop(msg)
+    }
+    length(check_DDA) < 3
 }
