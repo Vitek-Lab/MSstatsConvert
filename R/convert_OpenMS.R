7e84a80ec30ef353405087d252096928c4d2a072
mstaniak <mtst@mstaniak.pl>
2020-04-25 00:41:34 +0200

    updates OpenMS converter

--- R/convert_OpenMS.R
+++ R/convert_OpenMS.R
@@ -12,88 +12,10 @@
 #' @export
 #' 
 
-.OpenMSFromDFs = function(
-  input, annotation = NULL, useUniquePeptide = TRUE, fewMeasurements = "remove",
-  removeProtein_with1Feature = FALSE, summaryforMultipleRows = max,
-  use_log_file = TRUE, append = FALSE, verbose = TRUE
-) {
-  
-  fewMeasurements = .isLegalValue(fewMeasurements, c("remove", "keep"))
-  input = .cleanRawOpenMS(input)
-  
-  annotation = .makeAnnotation(
-    annotation, 
-    c("Run" = "Run", "Condition" = "Condition", "BioReplicate" = "BioReplicate"),
-    input
-  )
-  
-  feature_cols = c("PeptideSequence", "PrecursorCharge", "FragmentIon",
-                   "ProductCharge")
-  input = .handleSharedPeptides(input, useUniquePeptide)
-  input = .cleanByFeature(input, feature_cols, summaryforMultipleRows, 
-                          fewMeasurements)
-  input = .handleSingleFeaturePerProtein(input, removeProtein_with1Feature)
-  input = merge(input[, setdiff(colnames(input), c("Condition", "BioReplicate")), 
-                      with = FALSE], annotation, by = "Run")
-  input # ProteinName as factor?
-}
-
-
-.OpenMSFromFiles = function(
-  input, annotation = NULL, useUniquePeptide = TRUE, fewMeasurements = "remove",
-  removeProtein_with1Feature = FALSE, summaryforMultipleRows = max,
-  use_log_file = TRUE, append = FALSE, verbose = TRUE
-) {
-  
-}
-
-setGeneric("OpenMStoMSstatsFormat",
-           function(input, annotation, ...) {
-             standardGeneric("OpenMStoMSstatsFormat")
-           })
-
-setMethod("OpenMStoMSstatsFormat",
-          signature = rep("character", 2),
-          function(input, annotation, ...)
-            .OpenMSFromFiles(input, annotation, ...))
-
-
-setMethod("OpenMStoMSstatsFormat",
-          signature = rep("data.frame", 2),
-          function(input, annotation, ...) {
-            .OpenMSFromDFs(input, annotation, ...)
-          })
-
-setMethod("OpenMStoMSstatsFormat",
-          signature = c("data.frame", "missing"),
-          function(input, ...) {
-            .OpenMSFromDFs(input, ...)
-          })
-
-setMethod("OpenMStoMSstatsFormat",
-          signature = c("character", "missing"),
-          function(input, ...) {
-            .OpenMSFromFiles(input, ...)
-          })
-
-
-#' Import OpenMS files
-#' 
-#' @inheritParams .documentFunction
-#' @param input name of MSstats input report from OpenMS, which includes feature(peptide ion)-level data.
-#' @param annotation name of 'annotation.txt' data which includes Condition, BioReplicate, Run. 
-#' Run should be the same as filename.
-#' 
-#' @return data.frame with the required format of MSstats.
-#' 
-#' @author Meena Choi, Olga Vitek. 
-#' 
-#' @export
-#' 
-
 OpenMStoMSstatsFormat = function(
   input, annotation = NULL, useUniquePeptide = TRUE, fewMeasurements = "remove",
-  removeProtein_with1Feature = FALSE, summaryforMultipleRows = max
+  removeProtein_with1Feature = FALSE, summaryforMultipleRows = max,
+  use_log_file = TRUE, append = TRUE, verbose = TRUE
 ) {
   
   fewMeasurements = .isLegalValue(fewMeasurements, c("remove", "keep"))
@@ -110,16 +32,17 @@ OpenMStoMSstatsFormat = function(
   input = .handleSharedPeptides(input, useUniquePeptide)
   input = .cleanByFeature(input, feature_cols, summaryforMultipleRows, 
                           fewMeasurements)
-  input = .handleSingleFeaturePerProtein(input, removeProtein_with1Feature)
+  input = .handleSingleFeaturePerProtein(input, removeProtein_with1Feature,
+                                         feature_cols)
   input = merge(input[, setdiff(colnames(input), c("Condition", "BioReplicate")), 
                       with = FALSE], annotation, by = "Run")
-  input # ProteinName as factor?
+  input
 }
 
 
 .cleanRawOpenMS = function(om_input) {
-  colnames(om_input) = .standardizeColnames(om_input)
-  om_input = data.table::as.data.table(om_input)
+  om_input = .getDataTable(om_input)
+  colnames(om_input) = .standardizeColnames(colnames(om_input))
   om_input[["Intensity"]] = as.numeric(om_input[["Intensity"]])
   if(!is.element("IsotopeLabelType", colnames(om_input))) {
     om_input = .fillValues(om_input, c("IsotopeLabelType" = "L"))
