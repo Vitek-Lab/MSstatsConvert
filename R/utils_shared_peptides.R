ca7b8736ad24399af633cbabf1f02d50e1983057
mstaniak <mtst@mstaniak.pl>
2020-04-24 17:31:59 +0200

    updates code for shared peptides

---
+++ R/utils_shared_peptides.R
@@ -0,0 +1,28 @@
+#' Remove peptides assigned to more than one protein.
+#' @param input data.table pre-processed by one of the .cleanRaw* functions.
+#' @param protein_column chr, name of the column with names of proteins.
+#' @param peptide_column chr, name of the column with peptide sequences.
+#' @return data.table
+#' @keywords internal
+.removeSharedPeptides = function(input, protein_column, peptide_column) {
+    unique_pairs = unique(input[, c(protein_column, peptide_column), 
+                                with = FALSE])
+    protein_counts = unique_pairs[, .(count = .N), by = peptide_column]
+    protein_counts = protein_counts[protein_counts$count == 1L, 
+                                    peptide_column, with = FALSE]
+    merge(input, protein_counts)
+}
+
+#' Handle shared peptides.
+#' @inheritParams .removeSharedPeptides
+#' @param remove_shared lgl, if TRUE, shared peptides will be removed
+.handleSharedPeptides = function(input, remove_shared = TRUE,
+                                 protein_column = "ProteinName",
+                                 peptide_column = "PeptideSequence") {
+    if (remove_shared) {
+        input = .removeSharedPeptides(input, protein_column, peptide_column)
+        getOption("MSstatsLog")("INFO", "Shared peptides are removed")
+        getOption("MSstatsMsg")("INFO", "Shared peptides are removed")
+    } 
+    input
+}
