dbc2cb41868642ea6231138f1f7849c547054cdc
mstaniak <mtst@mstaniak.pl>
2020-04-25 01:23:41 +0200

    updates PD converter

--- R/convert_ProteomeDiscoverer.R
+++ R/convert_ProteomeDiscoverer.R
@@ -15,7 +15,7 @@
 #' @export
 #' 
 
-.PDFromDFs = function(
+PDtoMSstatsFormat = function(
     input, annotation, useNumProteinsColumn = FALSE, useUniquePeptide = TRUE,
     summaryforMultipleRows = max, fewMeasurements = "remove",
     removeOxidationMpeptides = FALSE, removeProtein_with1Peptide = FALSE,
@@ -39,7 +39,8 @@
                                   peptide_column = "PeptideModifiedSequence")
     input = .cleanByFeature(input, feature_cols, summaryforMultipleRows,
                             fewMeasurements)
-    input = .handleSingleFeaturePerProtein(input, removeProtein_with1Peptide)
+    input = .handleSingleFeaturePerProtein(input, removeProtein_with1Peptide,
+                                           feature_cols)
     input = merge(input, annotation, by = "Run")
     input = .fillValues(input, c("FragmentIon" = NA, "ProductCharge" = NA,
                                  "IsotopeLabelType" = "L"))
@@ -47,61 +48,32 @@
 }
 
 
-.PDFromFiles = function(
-    input, annotation, useNumProteinsColumn = FALSE, useUniquePeptide = TRUE,
-    summaryforMultipleRows = max, fewMeasurements = "remove",
-    removeOxidationMpeptides = FALSE, removeProtein_with1Peptide = FALSE,
-    which.quantification = 'Precursor.Area', 
-    which.proteinid = 'Protein.Group.Accessions', which.sequence = 'Sequence',
-    use_log_file = TRUE, append = FALSE, verbose = TRUE
-) {
-    
-}
-
-setGeneric("PDtoMSstatsFormat",
-           function(input, annotation, ...) {
-               standardGeneric("PDtoMSstatsFormat")
-           })
-
-setMethod("PDtoMSstatsFormat",
-          signature = rep("character", 2),
-          function(input, annotation, ...)
-              .PDFromFiles(input, annotation, ...))
-
-
-setMethod("PDtoMSstatsFormat",
-          signature = rep("data.frame", 2),
-          function(input, annotation, ...) {
-              .PDFromDFs(input, annotation, ...)
-          })
-
-
-
 .cleanRawPD = function(pd_input, quantification_column, proteinID_column,
                        sequence_column, filter_num_col) {
-    colnames(pd_input) = .standardizeColnames(pd_input)
-    which.quantification = .findAvailable(c("Intensity", "Area"),
-                                          colnames(pd_input),
-                                          "Precursor.Area")
-    which.quantification = .isLegalValue(which.quantification, 
-                                         legal_values = c("Intensity", "Area", "Precursor.Area",
-                                                          "Precursor.Abundance"),
-                                         message = "Please select a column to be used for quantified intensities among four options: ")
+    pd_input = .getDataTable(pd_input)
+    colnames(pd_input) = .standardizeColnames(colnames(pd_input))
+    quantification_column = .findAvailable(c("Intensity", "Area"),
+                                           colnames(pd_input),
+                                           quantification_column)
+    # which.quantification = .isLegalValue(which.quantification, 
+    #                                      legal_values = c("Intensity", "Area", "Precursor.Area",
+    #                                                       "Precursor.Abundance"),
+    #                                      message = "Please select a column to be used for quantified intensities among four options: ")
     
-    which.proteinid = .findAvailable(c("Protein.Accessions", 
-                                       "Master.Protein.Accessions"),
-                                     colnames(pd_input),
-                                     "Protein.Group.Accessions")
-    which.proteinid = .isLegalValue(which.proteinid, 
-                                    legal_values = c("ProteinAccessions", 
-                                                     "Master.Protein.Accessions",
-                                                     "Protein.Group.Accessions"),
-                                    message = "Please select a column to be used as protein IDs among three options: ")
-    which.sequence = .findAvailable("Annotated.Sequence", colnames(pd_input), 
-                                    "Sequence")
-    which.sequence = .isLegalValue(which.sequence, 
-                                   legal_values = c("Annotated.Sequence", "Sequence"),
-                                   message = "Please select peptide sequence column between two options: ")
+    proteinID_column = .findAvailable(c("Protein.Accessions", 
+                                        "Master.Protein.Accessions"),
+                                      colnames(pd_input),
+                                      proteinID_column)
+    # which.proteinid = .isLegalValue(which.proteinid, 
+    #                                 legal_values = c("ProteinAccessions", 
+    #                                                  "Master.Protein.Accessions",
+    #                                                  "Protein.Group.Accessions"),
+    #                                 message = "Please select a column to be used as protein IDs among three options: ")
+    sequence_column = .findAvailable("Annotated.Sequence", colnames(pd_input), 
+                                     sequence_column)
+    # which.sequence = .isLegalValue(which.sequence, 
+    #                                legal_values = c("Annotated.Sequence", "Sequence"),
+    #                                message = "Please select peptide sequence column between two options: ")
     
     if(filter_num_col) {
         pd_input = pd_input[pd_input[["X..Proteins"]] == '1', ]
@@ -111,13 +83,13 @@ setMethod("PDtoMSstatsFormat",
     if (any(is.element(colnames(pd_input), "Fraction"))) {
         pd_cols = c(pd_cols, "Fraction")
     }
-    input = data.table::as.data.table(pd_input[, pd_cols])
-    colnames(input) = .updateColnames(
-        input,
+    pd_input = pd_input[, pd_cols, with = FALSE]
+    colnames(pd_input) = .updateColnames(
+        pd_input,
         c(proteinID_column, sequence_column, "Spectrum.File", quantification_column),
         c("ProteinName", "PeptideSequence", "Run", "Intensity"))
-    input[["PeptideModifiedSequence"]] = paste(input[["PeptideSequence"]], 
-                                               input[["Modifications"]], 
-                                               sep = "_")
-    input[, -which(colnames(input) == "PeptideSequence")]
+    pd_input[["PeptideModifiedSequence"]] = paste(pd_input[["PeptideSequence"]], 
+                                                  pd_input[["Modifications"]], 
+                                                  sep = "_")
+    pd_input[, !(colnames(pd_input) == "PeptideSequence"), with = FALSE]
 }
