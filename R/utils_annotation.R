a445f89f8d69d5d216efd0e44d53aaee0e187b62
mstaniak <mtst@mstaniak.pl>
2020-04-27 17:19:44 +0200

    simplifies annotation-related code

--- R/utils_annotation.R
+++ R/utils_annotation.R
@@ -1,87 +1,32 @@
-.getChannelColumns = function(col_names, ...) {
-    all_patterns = unlist(list(...))
-    channel_filter = rep(TRUE, length(col_names))
-    for (pattern in all_patterns) {
-        channel_filter = channel_filter & grepl(pattern, col_names, fixed = TRUE)
-    }
-    col_names[channel_filter]
-}
-
-# COMMON DATA PREPROCESSING
-.checkColumnsGen = function(df_name, expected_columns, actual_columns, 
-                            lgl_fun, msg_sep) {
-    if (!lgl_fun(expected_columns %in% actual_columns)) {
-        missing_columns = setdiff(expected_columns, actual_columns)
-        stop(paste("Missing columns in", paste0(df_name, ":"), 
-                   paste(missing_columns, sep = msg_sep, collapse = msg_sep)))
-    }
-}
-
-.checkColumns = function(df_name, expected_columns, 
-                         actual_columns, type = "required") {
-    if (type == "required") {
-        .checkColumnsGen(df_name, expected_columns, actual_columns, all, ", ")    
+#' Create annotation
+#' @param input data.table preprocessed by one of the .cleanRaw* functions
+#' @param annotation data.table 
+#' @param ... key-value pairs, where keys are names of columns of `annotation` 
+.makeAnnotation = function(input, annotation, ...) {
+    all_columns = unlist(list(...))
+    annotation = .updateColnames(annotation, 
+                                 unname(all_columns),
+                                 names(all_columns))
+    joint_columns = intersect(colnames(input), colnames(annotation)) 
+    if (all(colnames(annotation) %in% joint_columns)) {
+        NULL
     } else {
-        .checkColumnsGen(df_name, expected_columns, actual_columns, any, " or ")        
+        annotation
     }
-    
+    # TODO: checks
 }
 
 
-.pickAnnotation = function(annotation, backup_annotation, columns_definition,
-                           backup_columns_definition) {
-    # Account for the case when user provides a path to file.
-    if (is.null(annotation)) {
-        .checkColumns("Annotation", names(backup_columns_definition), 
-                      colnames(backup_annotation))
-        list(df = backup_annotation,
-             cols = backup_columns_definition)
-    } else {
-        .checkColumns("Annotation", names(columns_definition), 
-                      colnames(annotation))
-        list(df = annotation,
-             cols = columns_definition)
-    }
-}
-
-.checkAnnotationValidity = function(annotation) {
-    counts_in_run = xtabs(~ Run, as.data.frame(annotation))
-    if (any(counts_in_run > 1)) {
-        stop('Please check annotation. Each MS Run must have a single condition and biological replicate')
-    }
-}
-
-#' @importFrom data.table fread as.data.table
-.makeAnnotation = function(annotation_source, columns_definition,
-                           backup_annotation_source = NULL,
-                           backup_columns_definition = NULL) {
-    if (is.null(annotation_source) & is.null(backup_annotation_source)) {
-        stop("Please provide annotation information")
-    }
-    if (is.null(backup_columns_definition) & !is.null(backup_annotation_source)) {
-        backup_columns_definition = columns_definition
-    }
-    if (is.character(annotation_source)) {
-        annotation_source = fread(annotation_source)
-    }
-    if (is.character(backup_annotation_source)) {
-        backup_annotation_source = fread(backup_annotation_source)
-    }
-    
-    annotation_list = .pickAnnotation(annotation_source, backup_annotation_source,
-                                      columns_definition, backup_columns_definition)
-    annotation_list[["df"]] = as.data.table(annotation_list[["df"]])
-    colnames(annotation_list[["df"]]) = .updateColnames(annotation_list[["df"]], 
-                                                        names(annotation_list[["cols"]]),
-                                                        annotation_list[["cols"]])
-    annotation_list[["df"]] = unique(annotation_list[["df"]][, annotation_list[["cols"]], with = FALSE])
-    .checkAnnotationValidity(annotation_list[["df"]])
-    annotation_list[["df"]]
-}
-
+#' Merge annotation with feature data
+#' @param data.table preprocessed by one of the .cleanRaw* functions.
+#' @param annotation data.table with annotation
+#' @return data.table
+#' @keywords internal 
 .mergeAnnotation = function(input, annotation) {
-    cols = intersect(colnames(input), colnames(annotation))
-    input = merge(input, annotation, by = cols, all.x = TRUE)
+    if (!is.null(annotation)) {
+        cols = intersect(colnames(input), colnames(annotation))
+        input = merge(input, annotation, by = cols, all.x = TRUE)
+    }
     if (any(is.na(input$Condition))) {
         msg = "Condition in the input file must match condition in annotation"
         getOption("MSstatsLog")("ERROR", msg)
