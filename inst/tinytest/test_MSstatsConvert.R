3a32bef9b3d32f90264353adf7af32c7f0addf83
mstaniak <mtst@mstaniak.pl>
2020-05-23 16:32:32 +0200

    adds tests

---
+++ inst/tinytest/test_MSstatsConvert.R
@@ -0,0 +1,26 @@
+data_invalid = data.table::data.table(ProteinName = letters[1:10],
+                                         PeptideSequence = LETTERS[1:10])
+data_no_channel = data.table::data.table(ProteinName = letters[1:10],
+                                         PeptideSequence = LETTERS[1:10],
+                                         IsotopeLabelType = "L")
+data_no_channel_LH = data.table::data.table(ProteinName = letters[1:10],
+                                            PeptideSequence = LETTERS[1:10],
+                                            IsotopeLabelType = rep(c("L", "H"),
+                                                                   each = 5))
+data_channel = data.table::data.table(ProteinName = letters[1:10],
+                                      PeptideSequence = LETTERS[1:10],
+                                      Channel = paste0("A", 1:10))
+tmt_object = MSstatsConvert:::.MSstatsFormat(data_channel)
+labelfree_object = MSstatsConvert:::.MSstatsFormat(data_no_channel)
+labelled_object = MSstatsConvert:::.MSstatsFormat(data_no_channel_LH)
+
+# Fails with data that is not in MSstats format
+expect_error(MSstatsConvert:::.MSstatsFormat(data_invalid))
+# Has the right format for MSstats data
+expect_true(is(labelled_object, "MSstatsLabeled"))
+expect_true(is(labelfree_object, "MSstatsLabelFree"))
+expect_true(is(tmt_object, "MSstatsTMT"))
+# Inherits after data.frame
+expect_true(is(labelled_object, "data.frame"))
+expect_true(is(labelfree_object, "data.frame"))
+expect_true(is(tmt_object, "data.frame"))
