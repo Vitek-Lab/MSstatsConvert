0025dc3626950bbc226cfcd16b93507ddfa1f5e6
Mateusz Staniak <mtst@mstaniak.pl>
2020-06-14 12:48:36 +0200

    removes derived classes

--- R/utils_classes.R
+++ R/utils_classes.R
@@ -3,34 +3,8 @@ setOldClass("data.frame")
 setClass("MSstatsValidated", contains = "data.frame")
 setOldClass("MSstatsValidated", S4Class = "MSstatsValidated")
 
-setClass("MSstatsLabelFree", contains = "MSstatsValidated")
-setOldClass("MSstatsLabelFree", S4Class = "MSstatsLabelFree")
-
-setClass("MSstatsLabeled", contains = "MSstatsValidated")
-setOldClass("MSstatsLabeled", S4Class = "MSstatsLabeled")
-setClass("MSstatsTMT", contains = "MSstatsValidated")
-setOldClass("MSstatsTMT", S4Class = "MSstatsTMT")
-
-
 #' @importFrom methods new
 #' @keywords internal
 .MSstatsFormat = function(input) {
-    if (is.element("Channel", colnames(input))) {
-        new("MSstatsTMT", new("MSstatsValidated", as.data.frame(input)))
-    } else {
-        if (is.element("IsotopeLabelType", colnames(input))) {
-            if (length(unique(input$IsotopeLabelType)) == 1L) {
-                new("MSstatsLabelFree", new("MSstatsValidated", 
-                                            as.data.frame(input)))
-            } else {
-                new("MSstatsLabeled", new("MSstatsValidated", 
-                                          as.data.frame(input)))
-            }
-        }
-        else {
-            msg = "Incorrect data format: missing isotope label information"
-            getOption("MSstatsLog")("ERROR", msg)
-            stop(msg)
-        }
-    }
+    new("MSstatsValidated", as.data.frame(input))
 }
