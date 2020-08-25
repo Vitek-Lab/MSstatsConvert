0c50e72f5bc98f835ea60c8f6e381eb4e7a82f22
Mateusz Staniak <mtst@mstaniak.pl>
2020-08-25 17:34:50 +0200

    fixes LF MaxQuant converter

--- R/convert_MaxQuant.R
+++ R/convert_MaxQuant.R
@@ -74,10 +74,10 @@
         mq_input$Intensity = ifelse(mq_input$Intensity == 0, NA, mq_input$Intensity)
         mq_input = .filterFewMeasurements(mq_input, 0, "keep", 
                                           c("PeptideSequence", "PrecursorCharge", "Run"))
+        mq_input = .filterFewMeasurements(mq_input, 0, "keep", 
+                                          c("PSM", "Run"))
     }
     
-    mq_input = mq_input[!is.na(mq_input[["Intensity"]]), ] # how about tmt case?
-    mq_input = .filterFewMeasurements(mq_input, 0, "keep", 
-                                      c("PSM", "Run"))
+    mq_input = mq_input[!is.na(mq_input[["Intensity"]]), ]
     mq_input
 }
