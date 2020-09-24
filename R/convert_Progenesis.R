d16688191e4fb3ae4bbc08190ae1a8c486a640ef
Mateusz Staniak <mtst@mstaniak.pl>
2020-09-25 00:29:16 +0200

    updates Progenesis converter
    Former-commit-id: d3815b3c65dfc2d804ac96dbc393e450dc0ed9b4

--- R/convert_Progenesis.R
+++ R/convert_Progenesis.R
@@ -32,18 +32,17 @@
         colnames(prog_input) = as.character(unlist(prog_input[1, ]))
         prog_input = prog_input[-1, ]
     }
+    colnames(prog_input) = .standardizeColnames(colnames(prog_input))
     protein_col = .findAvailable(c("Protein", "Accession"), colnames(prog_input))
     cols = which(colnames(prog_input) %in% c(protein_col, "Modifications", 
                                              "Sequence", "Charge",
-                                             "Use.in.quantitation"))
+                                             "Useinquantitation"))
     cols = c(cols, raw_abundances_col_ids)
     prog_input = prog_input[, cols, with = FALSE]
-    colnames(prog_input) = .standardizeColnames(colnames(prog_input))
     data.table::setnames(prog_input, 
                          c(protein_col, "Charge"), 
                          c("ProteinName", "PrecursorCharge"),
                          skip_absent = TRUE)
-    
     
     nonmissing_prot = !is.na(prog_input$ProteinName) & prog_input$ProteinName != ""
     nonmissing_pept = !is.na(prog_input$Sequence) & prog_input$Sequence != ""
