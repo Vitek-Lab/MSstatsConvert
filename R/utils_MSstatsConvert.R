e0fe9b115d0e344c63fc6b9963f66f4dc083c0b1
Mateusz Staniak <mtst@mstaniak.pl>
2020-09-26 13:14:49 +0200

    removes converters
    after moving them to MSstatsTMT and Mstats-dev
    
    Former-commit-id: f759381d46390b598f1abf4f72b89d352349b7a7

--- R/utils_MSstatsConvert.R
+++ R/utils_MSstatsConvert.R
@@ -8,29 +8,3 @@
 #' @name MSstatsConvert
 #'
 NULL
-
-#' A dummy function to store shared documentation items.
-#' 
-#' @import data.table
-#' 
-#' @param fewMeasurements 'remove'(default) will remove the features that have 1 or 2 measurements across runs.
-#' @param useUniquePeptide TRUE (default) removes peptides that are assigned for more than one proteins. 
-#' We assume to use unique peptide for each protein.
-#' @param summaryforMultipleRows max(default) or sum - when there are multiple measurements for certain feature and certain run, use highest or sum of multiple intensities.
-#' @param removeProtein_with1Feature TRUE will remove the proteins which have only 1 feature, which is the combination of peptide, precursor charge, fragment and charge. FALSE is default.
-#' @param removeProtein_with1Peptide TRUE will remove the proteins which have only 1 peptide and charge. FALSE is default.
-#' @param removeOxidationMpeptides TRUE will remove the peptides including 'oxidation (M)' in modification. FALSE is default.
-#' @param removeMpeptides TRUE will remove the peptides including 'M' sequence. FALSE is default.
-#' 
-#' @keywords internal
-#' 
-
-.documentFunction = function(fewMeasurements, 
-                             useUniquePeptide,
-                             summaryforMultipleRows, 
-                             removeProtein_with1Feature,
-                             removeProtein_with1Protein,
-                             removeOxidationMpeptides,
-                             removeMpeptides) {
-    NULL
-}
