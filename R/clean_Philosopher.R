1ef4e7f4e232705aaa0421ae9789f8b21299dd3e
Mateusz Staniak <mtst@mstaniak.pl>
2022-02-03 00:40:07 +0100

    updated Philosopher converter

--- R/clean_Philosopher.R
+++ R/clean_Philosopher.R
@@ -33,6 +33,7 @@
                            variable.name = "Channel", value.name = "Intensity",
                            variable.factor = FALSE)
   input[, Intensity := ifelse(Intensity == 0, NA, Intensity)]
+  input[, PSM := paste(PeptideSequence, PrecursorCharge, sep = "_")]
   input = input[!is.na(ProteinName) & ProteinName != ""]
   input
 }
