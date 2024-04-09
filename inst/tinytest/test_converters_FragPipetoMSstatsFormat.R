6bae8482acbeb614a2cd8899b0a43143187b80ca
tonywu1999 <ant.wu@northeastern.edu>
2024-04-09 12:27:33 -0400

    refactor(converter): Migrate core MSstats converter functions into MSstatsConvert (#91)
    * refactor(converter): Migrate core MSstats converter classes into MSstatsConvert
    
    * Add annotation files to MSstatsConvert package for pd, os, diau

---
+++ inst/tinytest/test_converters_FragPipetoMSstatsFormat.R
@@ -0,0 +1,18 @@
+# Test FragPipetoMSstatsFormat ---------------------------
+fragpipe_raw = system.file("tinytest/raw_data/FragPipe/fragpipe_input.csv",
+                              package = "MSstatsConvert")
+fragpipe_raw = data.table::fread(fragpipe_raw)
+output = FragPipetoMSstatsFormat(fragpipe_raw, use_log_file = FALSE)
+expect_equal(ncol(output), 11)
+expect_true(nrow(output) > 0)
+expect_true("Run" %in% colnames(output))
+expect_true("ProteinName" %in% colnames(output))
+expect_true("PeptideSequence" %in% colnames(output))
+expect_true("PrecursorCharge" %in% colnames(output))
+expect_true("Intensity" %in% colnames(output))
+expect_true("FragmentIon" %in% colnames(output))
+expect_true("ProductCharge" %in% colnames(output))
+expect_true("IsotopeLabelType" %in% colnames(output))
+expect_true("Condition" %in% colnames(output))
+expect_true("BioReplicate" %in% colnames(output))
+expect_true("Fraction" %in% colnames(output))
---
+++ inst/tinytest/test_converters_PDtoMSstatsFormat.R
@@ -0,0 +1,21 @@
+# Test PDtoMSstatsFormat ---------------------------
+pd_raw = system.file("tinytest/raw_data/PD/pd_input.csv",
+                     package = "MSstatsConvert")
+annot = system.file("tinytest/raw_data/PD/annot_pd.csv",
+                    package = "MSstatsConvert")
+pd_raw = data.table::fread(pd_raw)
+annot = data.table::fread(annot)
+output = PDtoMSstatsFormat(pd_raw, annot, use_log_file = FALSE)
+expect_equal(ncol(output), 11)
+expect_true(nrow(output) > 0)
+expect_true("Run" %in% colnames(output))
+expect_true("ProteinName" %in% colnames(output))
+expect_true("PeptideModifiedSequence" %in% colnames(output))
+expect_true("PrecursorCharge" %in% colnames(output))
+expect_true("Intensity" %in% colnames(output))
+expect_true("FragmentIon" %in% colnames(output))
+expect_true("ProductCharge" %in% colnames(output))
+expect_true("IsotopeLabelType" %in% colnames(output))
+expect_true("Condition" %in% colnames(output))
+expect_true("BioReplicate" %in% colnames(output))
+expect_true("Fraction" %in% colnames(output))
