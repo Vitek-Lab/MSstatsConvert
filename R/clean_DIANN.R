4752b7508fd0222d91126758a56461608f07c4ae
tonywu1999 <ant.wu@northeastern.edu>
2024-04-17 10:44:56 -0400

    fix(diann-converter): Allow for quantification column to be determined by client (#92)

--- R/clean_DIANN.R
+++ R/clean_DIANN.R
@@ -1,10 +1,12 @@
 #' Clean raw Diann files
 #' @param msstats_object an object of class `MSstatsDIANNFiles`.
 #' @param MBR True if analysis was done with match between runs
+#' @param quantificationColumn Use 'FragmentQuantCorrected'(default) column for quantified intensities. 'FragmentQuantRaw' can be used instead.
 #' @return data.table
 #' @importFrom stats na.omit
 #' @keywords internal
-.cleanRawDIANN = function(msstats_object, MBR = TRUE) {
+.cleanRawDIANN = function(msstats_object, MBR = TRUE, 
+                          quantificationColumn = "FragmentQuantCorrected") {
   dn_input = getInputFile(msstats_object, "input")
   dn_input = data.table::as.data.table(dn_input)
   
@@ -16,7 +18,7 @@
   }
   req_cols = c('ProteinNames', 'StrippedSequence', 
                'ModifiedSequence', 'PrecursorCharge',
-               'FragmentQuantCorrected', 'QValue', 
+               quantificationColumn, 'QValue', 
                'PrecursorMz', 'FragmentInfo', 'Run')
   if (MBR) {
     req_cols = c(req_cols, c('LibQValue', 'LibPGQValue'))
@@ -25,13 +27,13 @@
   }
   dn_input = dn_input[, req_cols, with = FALSE]
   dn_input = dn_input[, lapply(.SD, function(x) unlist(tstrsplit(x, ";"))),
-                      .SDcols = c("FragmentQuantCorrected", "FragmentInfo"), 
-                      by = setdiff(colnames(dn_input), c("FragmentInfo", "FragmentQuantCorrected"))]
+                      .SDcols = c(quantificationColumn, "FragmentInfo"), 
+                      by = setdiff(colnames(dn_input), c("FragmentInfo", quantificationColumn))]
   if (all(is.na(dn_input[["FragmentInfo"]]))) {
     dn_input[, FragmentInfo := paste0("Frag", 1:.N),
              by = c("ProteinNames", "ModifiedSequence", "PrecursorCharge", "Run")]
   }
-  dn_input[, FragmentQuantCorrected := as.numeric(FragmentQuantCorrected)]
+  dn_input[, (quantificationColumn) := lapply(.SD, as.numeric), .SDcols = quantificationColumn]
   dn_input[, FragmentIon := sub('\\^\\.\\*', '', FragmentInfo)]
   if (any(grepl("/", dn_input$FragmentInfo))) {
     dn_input[, ProductCharge := unlist(strsplit(FragmentInfo, split = "/"))[[1]], by = FragmentInfo]
@@ -41,10 +43,10 @@
   }
   dn_input = dn_input[!grepl("NH3", FragmentIon), ]
   dn_input = dn_input[!grepl("H2O", FragmentIon), ]
-  dn_input = na.omit(dn_input, cols = "FragmentQuantCorrected")
+  dn_input = na.omit(dn_input, cols = quantificationColumn)
   data.table::setnames(dn_input, old = c('ProteinNames', 'StrippedSequence', 
                                          'ModifiedSequence','PrecursorCharge',
-                                         'FragmentQuantCorrected', 'QValue', 
+                                         quantificationColumn, 'QValue', 
                                          'PrecursorMz', 'FragmentIon','Run', 
                                          'ProductCharge'),
                        new = c('ProteinName', 'PeptideSequence', 
