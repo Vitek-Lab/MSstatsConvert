14e7bd9d64f8c6072d5c01805bff24f302080dde
Tony Wu <wu.anthon@northeastern.edu>
2024-08-05 14:52:16 -0400

    add initial unit tests for protein prospector

---
+++ inst/tinytest/test_clean_ProteinProspector.R
@@ -0,0 +1,23 @@
+.testHappyPath = function(input) {
+    output = MSstatsConvert:::.cleanRawProteinProspector(input)
+    expect_equal(ncol(output), 7)
+    expect_equal(nrow(output), 966)
+    expected_column_names = c(
+        "ProteinName",
+        "PeptideSequence",
+        "PrecursorCharge",
+        "Run",
+        "PSM",
+        "Channel",
+        "Intensity"
+    )
+    missing_columns = setdiff(expected_column_names, colnames(output))
+    expect_equal(length(missing_columns), 0)
+}
+
+# Test PD .cleanRawPDTMT ---------------------------
+
+file_path = system.file("tinytest/processed_data/ProteinProspector/MSstatsProteinProspectorFilesObjectTMT.rds", package="MSstatsConvert")
+input = readRDS(file_path)
+.testHappyPath(input)
+
---
+++ inst/tinytest/test_converters_ProteinProspectortoMSstatsTMTFormat.R
@@ -0,0 +1,21 @@
+# Test ProteinProspectortoMSstatsTMTFormat ---------------------------
+input = system.file("tinytest/raw_data/ProteinProspector/Prospector_TotalTMT.txt",
+    package = "MSstatsConvert")
+input = data.table::fread(input)
+annot = system.file("tinytest/raw_data/ProteinProspector/Annotation.csv",
+                                package = "MSstatsConvert")
+annot = data.table::fread(annot)
+output = ProteinProspectortoMSstatsTMTFormat(input, annot)
+expect_equal(ncol(output), 11)
+expect_equal(nrow(output), 528)
+expect_true("Run" %in% colnames(output))
+expect_true("ProteinName" %in% colnames(output))
+expect_true("PeptideSequence" %in% colnames(output))
+expect_true("PrecursorCharge" %in% colnames(output))
+expect_true("Intensity" %in% colnames(output))
+expect_true("TechRepMixture" %in% colnames(output))
+expect_true("PSM" %in% colnames(output))
+expect_true("Mixture" %in% colnames(output))
+expect_true("Condition" %in% colnames(output))
+expect_true("BioReplicate" %in% colnames(output))
+expect_true("Channel" %in% colnames(output))
