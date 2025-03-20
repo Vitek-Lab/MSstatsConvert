1d0b9c112ce2cd59411a43f66d9e33be954afc0c
Tony Wu <wu.anthon@northeastern.edu>
2025-03-20 19:06:09 -0400

    add unit tests

--- inst/tinytest/test_converters_SpectronauttoMSstatsFormat.R
+++ inst/tinytest/test_converters_SpectronauttoMSstatsFormat.R
@@ -16,3 +16,42 @@ expect_true("IsotopeLabelType" %in% colnames(output))
 expect_true("Condition" %in% colnames(output))
 expect_true("BioReplicate" %in% colnames(output))
 expect_true("Fraction" %in% colnames(output))
+
+
+# Test SpectronauttoMSstatsFormat Missing Columns ---------------------------
+spectronaut_raw = system.file("tinytest/raw_data/Spectronaut/spectronaut_input.csv",
+                              package = "MSstatsConvert")
+spectronaut_raw = data.table::fread(spectronaut_raw)
+spectronaut_raw$F.ExcludedFromQuantification = NULL
+expect_error(
+    SpectronauttoMSstatsFormat(spectronaut_raw, use_log_file = FALSE),
+    "The following columns are missing from the input data: FExcludedFromQuantification"
+)
+
+spectronaut_raw = system.file("tinytest/raw_data/Spectronaut/spectronaut_input.csv",
+                              package = "MSstatsConvert")
+spectronaut_raw = data.table::fread(spectronaut_raw)
+spectronaut_raw$F.FrgLossType = NULL
+expect_error(
+    SpectronauttoMSstatsFormat(spectronaut_raw, use_log_file = FALSE),
+    "The following columns are missing from the input data: FFrgLossType"
+)
+
+spectronaut_raw = system.file("tinytest/raw_data/Spectronaut/spectronaut_input.csv",
+                              package = "MSstatsConvert")
+spectronaut_raw = data.table::fread(spectronaut_raw)
+spectronaut_raw$PG.ProteinGroups = NULL
+expect_error(
+    SpectronauttoMSstatsFormat(spectronaut_raw, use_log_file = FALSE),
+    "The following columns are missing from the input data: PGProteinGroups"
+)
+
+spectronaut_raw = system.file("tinytest/raw_data/Spectronaut/spectronaut_input.csv",
+                              package = "MSstatsConvert")
+spectronaut_raw = data.table::fread(spectronaut_raw)
+spectronaut_raw$FG.Charge = NULL
+expect_error(
+    SpectronauttoMSstatsFormat(spectronaut_raw, use_log_file = FALSE),
+    "The following columns are missing from the input data: FGCharge"
+)
+
