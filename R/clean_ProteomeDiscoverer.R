71e57c9f139a8d4c05d7584406f29ca740e59731
Tony Wu <wu.anthon@northeastern.edu>
2025-04-04 19:32:12 -0400

    fix bug

--- R/clean_ProteomeDiscoverer.R
+++ R/clean_ProteomeDiscoverer.R
@@ -131,7 +131,7 @@
       pd_input = pd_input[toupper(QuanInfo) == 'UNIQUE', ]
     }
   }
-  pd_input = pd_input[, !(colnames(pd_input) %in% "QuanInfo")]
+  pd_input = pd_input[, !(colnames(pd_input) %in% "QuanInfo"), with = FALSE]
   pd_input
 }
 
