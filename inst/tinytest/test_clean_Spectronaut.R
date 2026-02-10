3fb4464fc12140ddc611ba6fd04e3ecefc19cdd9
Tony Wu <wu.anthon@northeastern.edu>
2026-02-10 13:33:01 -0500

    enable ms1 quantity tests

---
+++ inst/tinytest/test_clean_Spectronaut.R
@@ -0,0 +1,11 @@
+# Test MS1Quantity value is used for intensity.
+spectronaut_raw = system.file("tinytest/raw_data/Spectronaut/spectronaut_input.csv",
+                              package = "MSstatsConvert")
+spectronaut_raw = data.table::fread(spectronaut_raw)
+spectronaut_raw$FG.MS1Quantity = 100000
+msstats_input = MSstatsConvert::MSstatsImport(
+    list(input = spectronaut_raw), "MSstats", "Spectronaut")
+output = MSstatsConvert:::.cleanRawSpectronaut(msstats_input, intensity = 'MS1Quantity', 
+                                    calculateAnomalyScores = FALSE, 
+                                    anomalyModelFeatures = c())
+expect_true(all(output$Intensity == 100000))
