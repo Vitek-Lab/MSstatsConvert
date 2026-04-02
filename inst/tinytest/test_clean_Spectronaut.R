6c90705b03cbb870fe1e36e9bf371fe2d7d1b2b2
Tony Wu <wu.anthon@northeastern.edu>
2026-04-02 14:51:02 -0400

    add unit tests

--- inst/tinytest/test_clean_Spectronaut.R
+++ inst/tinytest/test_clean_Spectronaut.R
@@ -14,3 +14,39 @@ expect_error(MSstatsConvert:::.cleanRawSpectronaut(msstats_input, intensity = 'i
                                                    calculateAnomalyScores = FALSE,
                                                    anomalyModelFeatures = c()),
              pattern = "not found in input data")
+
+# Tests for .assignSpectronautIsotopeLabelType
+make_spec_input = function(peptides) {
+    data.table::data.table(PeptideSequence = peptides)
+}
+
+dt = make_spec_input(c(
+    "_PEPTIDEK[Lys6]_",   # heavy via Lys6
+    "_PEPTIDER[Arg10]_",  # heavy via Arg10
+    "_PEPTIDEK_",         # light (has K, but no Lys6 label)
+    "_PEPTIDER_",         # light (has R, but no Arg10 label)
+    "_PEPTIDEAC_"         # NA (no K or R)
+))
+result = MSstatsConvert:::.assignSpectronautIsotopeLabelType(
+    dt, heavyLabels = c("K[Lys6]", "R[Arg10]"))
+expect_equal(result$IsotopeLabelType, c("H", "H", "L", "L", NA_character_))
+expect_equal(result$PeptideSequence,
+             c("_PEPTIDEK_", "_PEPTIDER_", "_PEPTIDEK_", "_PEPTIDER_", "_PEPTIDEAC_"))
+
+dt = make_spec_input(c(
+    "_PEPTM[Oxidation]IDEK_",   
+    "_S[Kmodification]PEPTIDEK_", 
+    "_PEPTM[Oxidation]IDEK[Lys6]_",
+    "_ACDEGFHI_"
+))
+result = MSstatsConvert:::.assignSpectronautIsotopeLabelType(
+    dt, heavyLabels = "K[Lys6]")
+expect_equal(result$IsotopeLabelType, c("L", "L", "H", NA_character_))
+# Non-heavy brackets and heavy label brackets are stripped from PeptideSequence
+expect_equal(result$PeptideSequence,
+             c("_PEPTM[Oxidation]IDEK_", "_S[Kmodification]PEPTIDEK_",
+               "_PEPTM[Oxidation]IDEK_", "_ACDEGFHI_"))
+
+dt = make_spec_input(c("_PEPTIDEK_", "_PEPTIDER_"))
+result = MSstatsConvert:::.assignSpectronautIsotopeLabelType(dt, heavyLabels = NULL)
+expect_equal(result, dt)
